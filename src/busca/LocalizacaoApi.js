b245d087792c7d262e05c3f013cf8aa1a0c15d2a
daniel almeida reis <daniel.7t@hotmail.com>
2022-11-02 15:36:25 -0300

    atualizacao

--- src/busca/LocalizacaoApi.js
+++ src/busca/LocalizacaoApi.js
@@ -1,12 +1,13 @@
 const BASE_URL = 'https://servicodados.ibge.gov.br/api/v1/';
 
+const responseTojson = (response) => response.json()
 
 export const fetchStates = async () => {
     const url = `${BASE_URL}/localidades/estados`;
-    return fetch(url).then(response =>response.json());
+    return fetch(url).then(responseTojson());
 }
 export const fetchCitiesForState = (state) =>{
     if(!state) return Promise.resolve([]);
     const url = `${BASE_URL}/localidades/estados/${state}/municipios`;
-    return fetch(url).then(response => response.json());
+    return fetch(url).then(responseTojson());
 }
